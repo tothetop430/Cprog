edd12ac8730f619abde7266066432f754e1fba1f
tothetop430 <jhuseyrer355545@gmail.com>
2019-10-25 23:20:56 +0530

    Made recursive code and added comments

--- fact.c
+++ fact.c
@@ -1,5 +1,5 @@
 /* Finding Factorial of a number*/
-#include <stdio.h>
+/*#include <stdio.h>
 int main()
 {
 	unsigned int n,f=1,i;
@@ -19,4 +19,30 @@ int main()
 	end:
 	printf("Negative numbers not accepted!");
 	return 0;
+}*/
+/* Factorial of a number using recurion approach*/
+#include<stdio.h>
+
+int fact(int);  // Declaration of function fact
+
+int main()
+{
+int number;
+printf("\nEnter a number to find its factorial : ");
+scanf("%d",&number);
+printf("Factorial of %d : %d",number,fact(number));
+return 0;
+}
+
+
+int fact(int number)
+{
+if(number==0)
+{
+return(1);
+}
+if(number>0)
+{
+return(number*fact(number-1));
+}
 }
